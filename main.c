7879a6c2c3a8619f1afd8eba1489af54aac4b472
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-10 15:57:45 -0300

    02.08 - Laços encadeados

--- main.c
+++ main.c
@@ -9,17 +9,32 @@ int main(){
     sprintf(palavraSecreta, "MELANCIA");
 
     int acertou=0, enforcou=0;
+    char chutes[26];
+    int tentativas=0;
 
     do {
-        char chute;
-        printf("Chute uma letra:");
-        scanf("%c", &chute);
 
         for (int i = 0; i < strlen(palavraSecreta); i++) {
-            if (palavraSecreta[i] == chute){
-            printf("A posição %d tem essa letra\n", i);
+            int achou=0;
+            for (int j = 0; j < tentativas; j++) {
+                if (chutes[j] == palavraSecreta[i]){
+                    achou=1;
+                    break;
+                }
+            }
+            if(achou){
+                printf("%c ", palavraSecreta[i]);
+            } else {
+                printf("_ ");
             }
         }
+        printf("\n");
+        char chute;
+        printf("Chute uma letra:");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
 
     } while (!acertou && !enforcou);
 
