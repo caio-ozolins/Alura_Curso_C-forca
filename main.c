24debddacda198ec2c6750d2d4af0f99cd01f99d
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-10 15:12:23 -0300

    02.06 - Percorrendo o array

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
     char palavraSecreta[20];
-    int acertou=0, enforcou=0;
 
     sprintf(palavraSecreta, "MELANCIA");
 
+    int acertou=0, enforcou=0;
+
     do {
-        //começar o jogo!
+        char chute;
+        printf("Chute uma letra:");
+        scanf("%c", &chute);
+
+        for (int i = 0; i < strlen(palavraSecreta); i++) {
+            if (palavraSecreta[i] == chute){
+            printf("A posição %d tem essa letra\n", i);
+            }
+        }
 
     } while (!acertou && !enforcou);
 
