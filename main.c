35814898c13697c9d92b02b0644181b2a61f54c6
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-10 15:02:41 -0300

    02.05 - O loop do-while

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char palavraSecreta[20];
+    int acertou=0, enforcou=0;
+
+    sprintf(palavraSecreta, "MELANCIA");
+
+    do {
+        //começar o jogo!
+
+    } while (!acertou && !enforcou);
+
+    return 0;
+}
