ace46c7f357d3f89021a3c0906a0e2fad1a009f2
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-22 11:35:46 -0300

    06.01 - Finalizando o jogo após erros seguidos

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ char palavraSecreta[20];
 char chutes[26];
 int tentativas=0;
 
-void cabecalho(){
+void cabecalho(){ // imprime o cabeçalho
     printf("|*****************|\n");
     printf("|* Jogo de Forca *|\n");
     printf("|*****************|\n\n");
@@ -21,7 +21,7 @@ void chuta(){ //chute do jogador
     tentativas++;
 }
 
-int jaChutou(char letra){
+int jaChutou(char letra){ // verifica se a letra já foi chutada
     int achou=0;
     for (int j = 0; j < tentativas; j++) {
         if (chutes[j] == letra){
@@ -32,7 +32,7 @@ int jaChutou(char letra){
     return achou;
 }
 
-void desenhaForca(){
+void desenhaForca(){ // imprime a palavra secreta
     for (int i = 0; i < strlen(palavraSecreta); i++) {
 
         int achou = jaChutou(palavraSecreta[i]);
@@ -46,26 +46,38 @@ void desenhaForca(){
     printf("\n");
 }
 
-void escolhePalavra(){
+void escolhePalavra(){ // escolhe a palavra secreta
     sprintf(palavraSecreta, "MELANCIA");
+}
+
+int enforcou(){
+    int erros=0;
+    for (int i = 0; i < tentativas; ++i) {
+        int existe = 0;
+        for (int j = 0; j < strlen(palavraSecreta); ++j) {
+            if (chutes[i] == palavraSecreta[j]){
+                existe = 1;
+                break;
+            }
+        }
+        if (!existe){erros++;}
+    }
+    return erros >= 5;
 }
 
 int main(){
 
-    int acertou=0, enforcou=0;
-
+    int acertou=0;
 
     escolhePalavra();
-
     cabecalho();
 
     do {
 
         desenhaForca();
-
         chuta();
 
-    } while (!acertou && !enforcou);
+    } while (!acertou && !enforcou());
 
     return 0;
 }
